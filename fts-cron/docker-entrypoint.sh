dc7b84fcc76bac1a4092d18d542a89fc59260d79
Thomas Beermann <thomas.beermann@cern.ch>
2019-07-25 15:47:08 +0200

    add configuration for dteam VO

--- fts-cron/docker-entrypoint.sh
+++ fts-cron/docker-entrypoint.sh
@@ -3,6 +3,9 @@
 if [[ $RUCIO_VO == 'atlas' ]]
 then
     j2 /renew_fts_proxy_atlas.sh.j2 > /renew_fts_proxy.sh
+elif [[ $RUCIO_VO == 'dteam' ]]
+then
+    j2 /renew_fts_proxy_dteam.sh.j2 > /renew_fts_proxy.sh
 else
     j2 /renew_fts_proxy.sh.j2 > /renew_fts_proxy.sh
 fi
