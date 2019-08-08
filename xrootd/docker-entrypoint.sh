6634d5562f83e527b011a3f9fa3e58bd0a45587c
Mario Lassnig <mario.lassnig@cern.ch>
2019-08-08 14:47:18 +0200

    add configurable port and enable TPC

--- xrootd/docker-entrypoint.sh
+++ xrootd/docker-entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-xrootd -d -R xrootd -n xrdrucio -c /etc/xrootd/xrdrucio.cfg
+echo "xrd.port $XRDPORT" >> /etc/xrootd/xrdrucio.cfg
+
+echo '======== /etc/xrootd/xrdrucio.cfg ========'
+cat /etc/xrootd/xrdrucio.cfg
+echo '=========================================='
+
+xrootd -d -R xrootd -c /etc/xrootd/xrdrucio.cfg
