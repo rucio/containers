8ec454e9a893a1a2e884bd6c26b1f63e4f03f94d
Mario Lassnig <mario.lassnig@cern.ch>
2019-08-09 20:33:27 +0200

    now with gsi

--- xrootd/docker-entrypoint.sh
+++ xrootd/docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "xrd.port $XRDPORT" >> /etc/xrootd/xrdrucio.cfg
-
 echo '======== /etc/xrootd/xrdrucio.cfg ========'
 cat /etc/xrootd/xrdrucio.cfg
 echo '=========================================='
 
-xrootd -d -R xrootd -c /etc/xrootd/xrdrucio.cfg
+echo 'Fixing ownership and permissions'
+chown -R xrootd:xrootd /etc/grid-security/xrd
+chmod 0400 /etc/grid-security/xrd/xrdkey.pem
+
+xrootd -R xrootd -n rucio -c /etc/xrootd/xrdrucio.cfg
