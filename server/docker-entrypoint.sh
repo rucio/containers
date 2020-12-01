f2cc71d8ab90cae31656b61aefe7cdd175a91f1d
Thomas Beermann <thomas.beermann@cern.ch>
2020-12-01 17:10:52 +0100

    add j2 customize file

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-j2 /tmp/00-mpm.conf.j2 > /etc/httpd/conf.modules.d/00-mpm.conf
+j2 --customize=/tmp/j2cfg.py /tmp/00-mpm.conf.j2 > /etc/httpd/conf.modules.d/00-mpm.conf
 
 echo "=================== /etc/httpd/conf.modules.d/00-mpm.conf ========================"
 cat /etc/httpd/conf.modules.d/00-mpm.conf
 echo ""
 
-j2 /tmp/httpd.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf/httpd.conf
+j2 --customize=/tmp/j2cfg.py /tmp/httpd.conf.j2 > /etc/httpd/conf/httpd.conf
 
 echo "=================== /etc/httpd/conf/httpd.conf ========================"
 cat /etc/httpd/conf/httpd.conf
@@ -16,7 +16,7 @@ if [ -f /opt/rucio/etc/rucio.cfg ]; then
     echo "rucio.cfg already mounted."
 else
     echo "rucio.cfg not found. will generate one."
-    j2 /tmp/rucio.cfg.j2 > /opt/rucio/etc/rucio.cfg
+    j2 --customize=/tmp/j2cfg.py /tmp/rucio.cfg.j2 > /opt/rucio/etc/rucio.cfg
 fi
 
 if [ ! -z "$RUCIO_PRINT_CFG" ]; then
@@ -25,7 +25,7 @@ if [ ! -z "$RUCIO_PRINT_CFG" ]; then
     echo ""
 fi
 
-j2 /tmp/rucio.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf.d/rucio.conf
+j2 --customize=/tmp/j2cfg.py /tmp/rucio.conf.j2 > /etc/httpd/conf.d/rucio.conf
 
 echo "=================== /etc/httpd/conf.d/rucio.conf ========================"
 cat /etc/httpd/conf.d/rucio.conf
