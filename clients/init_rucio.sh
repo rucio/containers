aaae2516ac3d98c5fb4b22d16f71e9a4834092bf
Thomas Beermann <thomas.beermann@cern.ch>
2020-12-08 10:03:59 +0100

    Revert "add default email config option + some rucio.cfg formatting changes"

--- clients/init_rucio.sh
+++ clients/init_rucio.sh
@@ -7,7 +7,7 @@ shopt -s checkwinsize
 if [ ! -f /opt/rucio/etc/rucio.cfg ]; then
     echo "File rucio.cfg not found. It will generate one."
     mkdir -p /opt/rucio/etc/
-    j2 --customize=/opt/user/j2cfg.py /opt/user/rucio.cfg.j2 > /opt/rucio/etc/rucio.cfg
+    j2 /opt/user/rucio.cfg.j2 > /opt/rucio/etc/rucio.cfg
 fi
 
 echo "Enable shell completion on the rucio commands"
