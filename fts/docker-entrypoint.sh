88ab9465477344dde3130cadcfd70d15dc113999
Mario Lassnig <mario.lassnig@cern.ch>
2021-03-18 16:56:28 +0100

    Update FTS version

--- fts/docker-entrypoint.sh
+++ fts/docker-entrypoint.sh
@@ -6,6 +6,9 @@
 # initialise / upgrade the database
 /usr/share/fts/fts-database-upgrade.py <<< y
 
+# fix Apache configuration
+/usr/bin/sed -i 's/Listen 80/#Listen 80/g' /etc/httpd/conf/httpd.conf
+
 # startup the FTS services
 /usr/sbin/fts_server               # main FTS server daemonizes
 /usr/sbin/httpd -DFOREGROUND       # FTS REST frontend & FTSMON
