d3f7c393d600d05629db3bc6533bd88efee89e3c
Radu Carpa <radu.carpa@cern.ch>
2022-08-10 14:12:44 +0200

    adapt installation and configuration for fts 3.12
    This was a major fts release which migrated to using python3 and
    had a major re-design of fts rest.
    Some scripts are broken because of that. They were not migrated yet.

--- fts/docker-entrypoint.sh
+++ fts/docker-entrypoint.sh
@@ -4,10 +4,14 @@
 /usr/local/bin/wait-for-it.sh -h ftsdb -p 3306 -t 3600
 
 # initialise / upgrade the database
-/usr/share/fts/fts-database-upgrade.py -y
+mysql -h ftsdb -u fts --password=fts fts < $(ls /usr/share/fts-mysql/fts-schema* | sort -t '-' -k '4n' | tail -n 1)
+# TODO: go back to using this script once the fixed it and bundled with the new fts version:
+# /usr/share/fts/fts-database-upgrade.py -y
 
 # fix Apache configuration
 /usr/bin/sed -i 's/Listen 80/#Listen 80/g' /etc/httpd/conf/httpd.conf
+cp /opt/rh/httpd24/root/usr/lib64/httpd/modules/mod_rh-python36-wsgi.so /lib64/httpd/modules
+cp /opt/rh/httpd24/root/etc/httpd/conf.modules.d/10-rh-python36-wsgi.conf /etc/httpd/conf.modules.d
 
 # startup the FTS services
 /usr/sbin/fts_server               # main FTS server daemonizes
