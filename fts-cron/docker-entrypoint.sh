8656e6232ea8136ab74319bdd5b6b46ab5260f14
Radu Carpa <radu.carpa@cern.ch>
2023-02-15 17:16:47 +0100

    fts-cron: prefer "script" variable over "vo" to pick the script
    This change builds on top of https://github.com/rucio/helm-charts/pull/139
    while being compatible with old helm chart versions.

--- fts-cron/docker-entrypoint.sh
+++ fts-cron/docker-entrypoint.sh
@@ -2,19 +2,24 @@
 
 echo "=================== Preparing renew_fts_proxy.sh ========================"
 
-if [[ $RUCIO_VO == 'atlas' ]]
+if [[ -z "$RUCIO_FTS_SCRIPT" ]]
+then
+    RUCIO_FTS_SCRIPT="$RUCIO_VO"
+fi
+
+if [[ $RUCIO_FTS_SCRIPT == 'atlas' ]]
 then
     j2 /opt/rucio/fts-delegate/renew_fts_proxy_atlas.sh.j2 > /opt/rucio/fts-delegate/renew_fts_proxy.sh
-elif [[ $RUCIO_VO == 'dteam' ]]
+elif [[ $RUCIO_FTS_SCRIPT == 'dteam' ]]
 then
     j2 /opt/rucio/fts-delegate/renew_fts_proxy_dteam.sh.j2 > /opt/rucio/fts-delegate/renew_fts_proxy.sh
-elif [[ $RUCIO_VO == 'multi_vo' ]]
+elif [[ $RUCIO_FTS_SCRIPT == 'multi_vo' ]]
 then
     j2 /opt/rucio/fts-delegate/renew_fts_proxy_multi_vo.sh.j2 > /opt/rucio/fts-delegate/renew_fts_proxy.sh
-elif [[ $RUCIO_VO == 'tutorial' ]]
+elif [[ $RUCIO_FTS_SCRIPT == 'tutorial' ]]
 then
     j2 /opt/rucio/fts-delegate/renew_fts_proxy_tutorial.sh.j2 > /opt/rucio/fts-delegate/renew_fts_proxy.sh
-elif [[ $RUCIO_VO == 'escape' ]]
+elif [[ $RUCIO_FTS_SCRIPT == 'escape' ]]
 then
     j2 /opt/rucio/fts-delegate/renew_fts_proxy_escape.sh.j2 > /opt/rucio/fts-delegate/renew_fts_proxy.sh
 else
