5ba66206ff7af74d096c7c543ab374b07961e3e6
Radu Carpa <radu.carpa@cern.ch>
2023-03-23 11:22:01 +0100

    daemons: use openssl 3.1 by default
    Can still be disabled by setting the env variable
    USE_DAVIX_WITH_OPENSSL31 to anything != 'True'

--- daemons/start-daemon.sh
+++ daemons/start-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ ! -z "$USE_DAVIX_WITH_OPENSSL31" ]; then
+if [ "$USE_DAVIX_WITH_OPENSSL31" = 'True' ]; then
     echo "=================== installing davix with openssl 3.1 ============================"
     rpm -i /tmp/rpms/openssl31-libs-3*.rpm
     rpm -U --force /tmp/rpms/davix-libs-0*.rpm
