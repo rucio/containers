fa02b3cb387d76310a763f9cc3dc28d22f95859f
maany <imptodefeat@gmail.com>
2024-06-28 14:01:23 +0200

    ui: add patches and CA trust anchors #331

--- ui/docker-entrypoint.sh
+++ ui/docker-entrypoint.sh
@@ -32,6 +32,62 @@ echo "=================== /etc/httpd/conf.d/rucio.conf ========================"
 cat /etc/httpd/conf.d/rucio.conf
 echo ""
 
+if [ -d "/patch" ]
+then
+    echo "Patches found. Trying to apply them"
+
+    TMP_PATCH_DIR="$(mktemp -d)"
+    trap 'rm -rf -- "$TMP_PATCH_DIR"' EXIT # Deletes temp dir when script exits
+
+    for patchfile in /patch/*.patch
+    do
+        echo "Applying patch ${patchfile}"
+        
+        tmp_bin_file="${TMP_PATCH_DIR}/tmp_bin"
+
+        if ! filterdiff -i '*/bin/*' "${patchfile}" > ${tmp_bin_file}; then
+            echo "Error while filtering patch ${patchfile}/bin. Exiting setup."
+            exit 1
+        fi
+
+        if [ -s ${tmp_bin_file} ]
+        then
+            if patch -p2 -d "/usr/local/bin/" < ${tmp_bin_file}
+            then
+                echo "Patch ${patchfile}/bin applied."
+            else
+                echo "Patch ${patchfile}/bin could not be applied successfully (exit code $?). Exiting setup."
+                exit 1
+            fi
+        fi
+
+        tmp_lib_file="${TMP_PATCH_DIR}/tmp_lib"
+
+        if ! filterdiff -i '*/lib/*' "${patchfile}" > ${tmp_lib_file}; then
+            echo "Error while filtering patch ${patchfile}/lib. Exiting setup."
+            exit 1
+        fi
+
+        if [ -s ${tmp_lib_file} ]
+        then
+            if patch -p3 -d "$RUCIO_PYTHON_PATH" < ${tmp_lib_file}
+            then
+                echo "Patch ${patchfile}/lib applied."
+            else
+                echo "Patch ${patchfile}/lib could not be applied successfully (exit code $?). Exiting setup."
+                exit 1
+            fi
+        fi
+    done
+fi
+
+if [ -d /etc/grid-security/certificates ]; then
+    echo 'Adding Grid CAs to the system trust.'
+    cp -v /etc/grid-security/certificates/*.pem /etc/pki/ca-trust/source/anchors/
+    update-ca-trust extract
+fi
+
+
 pkill httpd || :
 sleep 2
 exec httpd -D FOREGROUND
