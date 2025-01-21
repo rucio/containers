d9d652f023e055a9b79ea44d9e143eac8d1eb724
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2025-01-21 14:25:17 +0100

    Add option to verify patching without executing daemon.
    Fixes #362

--- daemons/start-daemon.sh
+++ daemons/start-daemon.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
 
-if [ "$USE_DAVIX_WITH_OPENSSL31" = 'True' ]; then
-    echo "=================== installing davix with openssl 3.1 ============================"
-    rpm -i /tmp/rpms/openssl31-libs-3*.rpm
-    rpm -U --force /tmp/rpms/davix-libs-0*.rpm
-fi
-
-if [ -f /opt/rucio/etc/rucio.cfg ]; then
-    echo "rucio.cfg already mounted."
-else
-    echo "rucio.cfg not found. will generate one."
-    python3 /usr/local/rucio/tools/merge_rucio_configs.py \
-        -s /tmp/rucio.config.default.cfg $RUCIO_OVERRIDE_CONFIGS \
-        --use-env \
-        -d /opt/rucio/etc/rucio.cfg
-fi
-
-if [ ! -z "$RUCIO_PRINT_CFG" ]; then
-    echo "=================== /opt/rucio/etc/rucio.cfg ============================"
-    cat /opt/rucio/etc/rucio.cfg
-    echo ""
-fi
-
-RUCIO_PYTHON_PATH=$(python3 -c "import os; import rucio; print(os.path.dirname(rucio.__file__))")
-
-/usr/bin/memcached -u memcached -p 11211 -m 64 -c 1024 &
-
-if [ "$RUCIO_DAEMON" == "hermes" ]
-then
-  echo "starting sendmail for $RUCIO_DAEMON"
-  sendmail -bd
-fi
-
+apply_patches() {
 if [ -d "/patch" ]
 then
     echo "Patches found. Trying to apply them"
@@ -42,9 +11,9 @@ then
     for patchfile in /patch/*.patch
     do
         echo "Applying patch ${patchfile}"
-        
+
         tmp_bin_file="${TMP_PATCH_DIR}/tmp_bin"
-        
+
         if ! filterdiff -i '*/bin/*' "${patchfile}" > ${tmp_bin_file}; then
             echo "Error while filtering patch ${patchfile}/bin. Exiting setup."
             exit 1
@@ -79,6 +48,49 @@ then
             fi
         fi
     done
+else
+  echo "Patches not found"
+fi
+}
+
+if [ "$USE_DAVIX_WITH_OPENSSL31" = 'True' ]; then
+    echo "=================== installing davix with openssl 3.1 ============================"
+    rpm -i /tmp/rpms/openssl31-libs-3*.rpm
+    rpm -U --force /tmp/rpms/davix-libs-0*.rpm
+fi
+
+if [ -f /opt/rucio/etc/rucio.cfg ]; then
+    echo "rucio.cfg already mounted."
+else
+    echo "rucio.cfg not found. will generate one."
+    python3 /usr/local/rucio/tools/merge_rucio_configs.py \
+        -s /tmp/rucio.config.default.cfg $RUCIO_OVERRIDE_CONFIGS \
+        --use-env \
+        -d /opt/rucio/etc/rucio.cfg
+fi
+
+if [ ! -z "$RUCIO_PRINT_CFG" ]; then
+    echo "=================== /opt/rucio/etc/rucio.cfg ============================"
+    cat /opt/rucio/etc/rucio.cfg
+    echo ""
+fi
+
+RUCIO_PYTHON_PATH=$(python3 -c "import os; import rucio; print(os.path.dirname(rucio.__file__))")
+
+if [[ "${RUCIO_DAEMON_ONLY_CHECK_PATCHES}" ]];
+  then
+    apply_patches
+    exit 0
+fi
+
+/usr/bin/memcached -u memcached -p 11211 -m 64 -c 1024 &
+
+apply_patches
+
+if [ "$RUCIO_DAEMON" == "hermes" ]
+then
+  echo "starting sendmail for $RUCIO_DAEMON"
+  sendmail -bd
 fi
 
 if [ -d /etc/grid-security/certificates ]; then
